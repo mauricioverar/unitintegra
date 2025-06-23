5669af655b4784340d696a4f930f5651a70dc5d5
Mauricio <stb539@gmail.com>
2025-06-23 18:44:14 -0400

    lambda -> return

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -12,6 +15,18 @@ public class Main {
 
         int resultado = service.procesarOperacion(4, 5);
         System.out.println("Resultado de la operación: " + resultado);//18
+
+        // lambda
+        //  tienes una lista de números y quieres imprimirlos:
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4);
+        //numeros.forEach(n -> System.out.println(n)); // lambda
+        // ó
+        numeros.forEach(System.out::println); // metodo referencia
+
+        //Aquí, n -> System.out.println(n) es una lambda que se pasa al métdo forEach.
     }
 
+    // ejemplo lambda
+    // (int a, int b) -> a + b
+    //Esto representa una función que recibe dos enteros y devuelve su suma.
 }
