4678ed362fb88cf8cd3f06d561bae6acc8deb2f7
Mauricio <stb539@gmail.com>
2025-06-23 18:18:53 -0400

    instanciar a la clase Calculadora y ejecutar metodo o funcion restar

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
+
         Calculadora calc = new Calculadora();
         OperacionService service = new OperacionService(calc);
 
+        int resta = calc.restar(4, 5);
+        System.out.println("Resultado de la resta: " + resta);//-1
+
         int resultado = service.procesarOperacion(4, 5);
-        System.out.println("Resultado de la operación: " + resultado);
+        System.out.println("Resultado de la operación: " + resultado);//18
     }
 
 }
